f111aab6c5b17bca40e039a75c9d9ed312da18f4
titine63 <sblanchonnet@yahoo.fr>
2023-06-27 15:45:32 +0200

    challenge08

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const express = require("express");
-const { hashPassword } = require("./auth.js");
+const { hashPassword, verifyPassword, verifyToken } = require("./auth");
 
 const app = express();
 
@@ -15,21 +15,57 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
+const userHandlers = require("./userHandlers");
 const movieHandlers = require("./movieHandlers");
 
+// Routes publiques (GET)
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+app.get("/api/users", userHandlers.getUsers);
+app.get("/api/users/:id", userHandlers.getUserById);
+
+// Routes publiques (POST)
+app.post("/api/users", hashPassword, userHandlers.postUser);
+app.post(
+  "/api/login",
+  userHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+// Middleware de vérification du token pour les routes protégées (POST, PUT, DELETE)
+app.use(verifyToken);
+
+// Routes protégées (POST, PUT, DELETE)
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
-const userHandlers = require("./userHandlers");
+app.put(
+  "/api/users/:id",
+  (req, res, next) => {
+    // Vérifier si l'ID correspond à celui du payload du token
+    if (req.params.id !== req.payload.sub) {
+      return res.status(403).send("Forbidden");
+    }
+    // Si les IDs correspondent, passer à la logique de traitement
+    next();
+  },
+  hashPassword,
+  userHandlers.updateUser
+);
 
-app.get("/api/users", userHandlers.getUsers);
-app.get("/api/users/:id", userHandlers.getUserById);
-app.post("/api/users", hashPassword, userHandlers.postUser);
-app.put("/api/users/:id", hashPassword, userHandlers.updateUser);
-app.delete("/api/users/:id", userHandlers.deleteUser);
+app.delete(
+  "/api/users/:id",
+  (req, res, next) => {
+    // Vérifier si l'ID correspond à celui du payload du token
+    if (req.params.id !== req.payload.sub) {
+      return res.status(403).send("Forbidden");
+    }
+    // Si les IDs correspondent, passer à la logique de traitement
+    next();
+  },
+  userHandlers.deleteUser
+);
 
 app.listen(port, (err) => {
   if (err) {
